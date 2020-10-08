b9419d458e3c882e46661a51f13beec29832520c
millerman86 <amrenmiller@gmail.com>
2020-10-08 16:25:14 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const expressjwt = require('express-jwt')
+const cors = require('cors')
+
+const app = express()
+require('dotenv').config()
+
+
+app.use(cors())
+app.use(express.json())
+app.use(morgan('dev'))
+
+mongoose.connect(
+    process.env.DATABASE_URL || 'mongodb://localhost:27017/full-stack-app', // all collections will go into one database entry
+    {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true, 
+        useCreateIndex: true,
+        useFindAndModify: false
+    }, 
+    () => console.log('Connected to the DB')
+)
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+
+app.listen(process.env.PORT || 9000, () => {
+    console.log('Server is running on local port 9000');
+})
