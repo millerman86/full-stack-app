e36f8fcafb5cb71ae67a1bf96322fe68112573a7
millerman86 <amrenmiller@gmail.com>
2020-10-09 18:43:22 -0600

    test no loop

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,25 @@ import React from 'react';
 import './App.css';
 import {Route, Switch, Router as BrowserRouter} from 'react-router-dom'
 import axios from 'axios'
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 
 function App() {
 
   const [comments, setComments] = useState([])
 
-  axios.get('/api/comment')
-    .then(res => {
-      console.log('here is your data', res.data);
-      setComments(res.data)
-      console.log(res.data)
-    })
-    .catch(err => {
-      console.log(err);
-    })
+  useEffect(() => {
+    axios.get('/api/comment')
+      .then(res => {
+        console.log('here is your data', res.data);
+        setComments(res.data)
+        console.log(res.data)
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }, [])
+
+ 
 
   return (
     <div>
