7494194a71a77acc332352ba7c828d263bee1160
millerman86 <amrenmiller@gmail.com>
2020-10-09 20:55:01 -0600

    no uri

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://amren:amren@amcluster.2k38f.mongodb.net/test?retryWrites=true&w=majority', 
     secretOrKey: 'SECRET'
 }
 
--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,3 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://amren:amren@amcluster.2k38f.mongodb.net/<dbname>?retryWrites=true&w=majority', 
     secretOrKey: process.env.SECRET_OR_KEY
 }
