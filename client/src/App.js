50c7596d3c488aba41d5cdc07a361db9b283f21e
millerman86 <amrenmiller@gmail.com>
2020-10-09 18:10:47 -0600

    test

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import axios from 'axios'
 
 function App() {
 
-  const comments = axios.get('/comment')
+  const comments = axios.get('/api/comment')
     .then(res => {
       console.log('here is your data', res.data);
       console.log(res.data)
