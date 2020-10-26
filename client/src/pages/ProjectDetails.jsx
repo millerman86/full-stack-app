0386fc32963c93394faee0342a84beb95ac42b44
millerman86 <amrenmiller@gmail.com>
2020-10-26 11:44:36 -0600

    Finish with ticket-details

--- client/src/pages/ProjectDetails.jsx
+++ client/src/pages/ProjectDetails.jsx
@@ -1,5 +1,78 @@
 import React from 'react'
 
-export default function () {
-    return (<div>hello world</div>)
+
+export default () => {
+    useRouteMatch('/df')
+    return (
+    <div>
+        <div>
+            <header className="details-for-project">
+                <h1>Details for Project #1</h1>
+                <a href="back-to-list">Back to List</a> | <a href="Edit">Edit</a>
+            </header>
+            <div>
+                <div>
+                    <h3>Project Name</h3>
+                    <h1>Demo Project 1</h1>
+                </div>
+                <div>
+                    <h3>Project Description</h3> 
+                    <h1>This is a project number 1</h1>
+                </div>
+            </div>
+        </div>
+        <div>
+            <div className="assigned-personnel">
+                <header></header>
+                Show {10} Entries 
+
+                <input type="text"/>
+                <table>
+                    <tr>
+                        <th>User Name</th>
+                        <th>E-mail</th>
+                        <th>Role</th>
+                    </tr>
+                    <tr>
+                        <td>January</td>
+                        <td>$100</td>
+                        <td>$100</td>
+                    </tr>
+                    <tr>
+                        <td>February</td>
+                        <td>$100</td>
+                        <td>$80</td>
+                    </tr>
+                </table>
+                <div>
+                    <div>Showing 1 to 4 of 4 entries</div>
+                    <div><span>Previous</span><span>1</span><span>Next</span></div>
+                </div>
+            </div>
+            <div className="tickets-for-this-project">
+                <header></header>
+                Show {10} Entries
+
+                <input type="text"/>
+                <table>
+                    <tr>
+                        <th>Month</th>
+                        <th>Savings</th>
+                    </tr>
+                    <tr>
+                        <td>January</td>
+                        <td>$100</td>
+                    </tr>
+                    <tr>
+                        <td>February</td>
+                        <td>$80</td>
+                    </tr>
+                </table>
+                <div>
+                    <div>Showing 1 to 4 of 4 entries</div>
+                    <div><span>Previous</span><span>1</span><span>Next</span></div>
+                </div>
+            </div>
+        </div>
+    </div>)
 }
--- client/src/pages/TicketDetails.jsx
+++ client/src/pages/TicketDetails.jsx
@@ -1,5 +1,141 @@
 import React from 'react'
 
-export default function () {
-    return (<div>hello world</div>)
+export default () => {
+    return (
+    <div>
+
+        <div className="ticket-details-and-comments-flex">
+            <div className="left-column"> 
+                <header>
+                    <h1>Details for Ticket {ticketNumber}</h1>
+                    <a href="" className="back-to-list">Back to List</a> | <a href="" className="edit-ticket">Edit Ticket</a>
+                </header>
+                <div>
+                    <div>
+                        <h3 className="ticket-title">TICKET TITLE</h3>
+                        <p className="ticket-title-text">Great Work</p>
+                    </div>
+                    <div>
+                        <h3 className="ticket-description">TICKET DESCRIPTION</h3>
+                        <p className="ticket-description-text">Keep on plugging in the code, you're getting there</p>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <h3 className="assigned-developer">ASSIGNED DEVELOPER</h3>
+                        <p className="assigned-developer-role">DemoD Dev</p>
+                    </div>
+                    <div>
+                        <h3 className="submitter">SUBMITTER</h3>
+                        <p className="submitter-role">DemoS Submit</p>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <h3 className="project">PROJECT</h3>
+                        <p className="project-title">Demo Project 1</p>
+                    </div>
+                    <div>
+                        <h3 className="ticket-priority">TICKET PRIORITY</h3>
+                        <p className="ticket-priority-level">Medium</p>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <h3 className="ticket-status">TICKET STATUS</h3>
+                        <p className="ticket-status-state">Open</p>
+                    </div>
+                    <div>
+                        <h3 className="ticket-type">TICKET TYPE</h3>
+                        <p className="ticket-type-description"></p>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <h3 className="created">CREATED</h3>
+                        <p className="created-date">11/18/2019 9:54:45AM</p>
+                    </div>
+                    <div>
+                        <h3 className="updated">UPDATED</h3>
+                        <p className="updated-lastupdated"></p>
+                    </div>
+                </div>
+            </div>
+            <div className="right-column">
+                <p className="add-a-comment">Add a Comment?</p>
+                <div>
+                    <input type="text"/><button>Add</button>
+                </div>
+                <div>
+                    <header></header>
+                    <span>Show {10} entries</span> 
+    
+                    <input type="text"/>
+                    <table>
+                        <tr>
+                            <th className="commenter">Commenter</th>
+                            <th className="message">Message</th>
+                            <th className="created">Created</th>
+                        </tr>
+                        <tr>
+                            <td className="commenter-data"></td>
+                            <td className="message-data"></td>
+                            <td className="created-data"></td>
+                        </tr>
+                    </table>
+                    <div>
+                        <div>Showing 0 to 0 of 0 entries</div>
+                        <div><span>Previous</span><span>Next</span></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <hr/>
+
+        <div className="ticket-history-and-attachment-flex">
+            <div>
+                <header></header>
+                <span>Show {10} entries</span> 
+                <input type="text"/>
+    
+                <table>
+                    <tr>
+                        <th className="property">Property</th>
+                        <th className="old-value">Old Value</th>
+                        <th className="new-value">New Value</th>
+                        <th className="date-changed">Date Changed</th>
+                    </tr>
+                    <tr>
+                        <td className="property-data"></td>
+                        <td className="old-value-data"></td>
+                        <td className="new-value-data"></td>
+                        <td className="date-changed-data"></td>
+                    </tr>
+                </table>
+    
+                <div className="show-entries">
+                    <div>Showing 1 to 3 of 3 entries</div>
+                    <div><span>Previous</span><span>1</span><span>Next</span></div>
+                </div>
+            </div>
+            <div>
+                <table>
+                    <tr>
+                        <th className="file">File</th>
+                        <th className="uploader">Uploader</th>
+                        <th className="notes">Notes</th>
+                        <th className="created">Created</th>
+                    </tr>
+                    <tr>
+                        <td className="file-data"></td>
+                        <td className="uploader-data"></td>
+                        <td className="notes-data"></td>
+                        <td className="created-data"></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+       
+    </div>)
 }
