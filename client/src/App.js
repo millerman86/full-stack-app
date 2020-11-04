202d472be164479bbd495773f149b3962e079ea3
millerman86 <amrenmiller@gmail.com>
2020-11-04 07:47:08 -0700

    Finish sidenav and added icons

--- client/src/App.js
+++ client/src/App.js
@@ -33,6 +33,7 @@ import ProjectDetails from './pages/ProjectDetails'
 import TicketDetails from './pages/TicketDetails'
 import TicketHistoryAndAttachment from './pages/TicketHistoryAndAttachment'
 import YourTickets from './pages/YourTickets'
+import { FaUser, FaBell, FaUsers, FaUserTag, FaLayerGroup, FaTicketAlt, FaHome } from 'react-icons/fa'
 
 
 function App(props) {
@@ -41,77 +42,136 @@ function App(props) {
 
   return (
     <main>
-      <header className="app-header">
-          <div>Logged in as: <span className="admin">Admin</span></div>
-          <div className="global-search">
-            <input type="text" placeholder="Search" />
-            <span>NOTIFICATIONS<i /></span>
-            <span>USER ACTIONS<i /></span>
+
+      
+      <div id="mySidebar" className="sidebar">
+              
+          <div className="sidebar-flex">
+            <div>
+              <FaUser className="fa-user">
+    
+              </FaUser>
+            </div>
+            <div className="welcome-user">
+                <div>
+                  <p>Welcome</p>
+                  <p>{'amren'}</p>
+                </div>
+            </div>
           </div>
-      </header>
-      <Router>
-          <Switch>
-            <Route 
-              exact path="/" 
-              render={() => (token ? <Redirect to="/dashboardsplash" /> : <Auth />)}
-            />
-            <ProtectedRoute
-              // THIS MIGHT BE '/'
-              path="/demouserlogin"
-              component={DemoUserLogin}
-              redirectTo="/"
-              token={token}
-            />
-            <ProtectedRoute
-              path="/createnewproject"
-              component={CreateNewProject}
-              redirectTo="/"
-              token={token}
-            />
-            <ProtectedRoute
-              path="/dashboardsplash"
-              component={DashboardSplash}
-              redirectTo="/"
-              token={token}
-            />
-            <ProtectedRoute
-              path="/editticket"
-              component={EditTicket}
-              redirectTo="/"
-              token={token}
-            />
-            <ProtectedRoute
-              path="/manageuserroles"
-              component={ManageUserRoles}
-              redirectTo="/"
-              token={token}
-            />
-            <ProtectedRoute
-              path="/projectdetails"
-              component={ProjectDetails}
-              redirectTo="/"
-              token={token}
-            />
-            <ProtectedRoute
-              path="/ticketdetails"
-              component={TicketDetails}
-              redirectTo="/"
-              token={token}
-            />
-            <ProtectedRoute
-              path="/tickethistoryandattachment"
-              component={TicketHistoryAndAttachment}
-              redirectTo="/"
-              token={token}
-            />
-            <ProtectedRoute
-              path="/yourtickets"
-              component={YourTickets}
-              redirectTo="/"
-              token={token}
-            />
-          </Switch>
-      </Router>
+          
+          <div className="sidebar-navlink">
+            <span><FaHome /></span>
+            <span href="#">Dashboard Home</span>
+          </div>
+          <div className="sidebar-navlink">
+            <span><FaUserTag /></span>
+            <span href="#">Manage Role Assignment</span>
+          </div>
+          <div className="sidebar-navlink">
+            <span><FaUsers /></span>
+            <span href="#">Manage Project Users</span>
+          </div>
+          <div className="sidebar-navlink">
+            <span><FaLayerGroup /></span>
+            <span href="#">My Projects</span>
+          </div>
+          <div className="sidebar-navlink">
+            <span><FaTicketAlt /></span>
+            <span href="#">My Tickets</span>
+          </div>
+          <div className="sidebar-navlink">
+            <span><FaUser /></span>
+            <span href="#">My User Profile</span>
+          </div>
+          
+      </div>
+      
+      
+      <div className="main-content">
+
+          <header className="app-header">
+              <div>
+                Logged in as: <span className="admin">Admin</span>
+              </div>
+              <div className="global-search">
+                <input type="text" placeholder="Search" />
+                <span><FaHome /></span>
+                <span>NOTIFICATIONS <FaBell /></span>
+                <span>USER ACTIONS <FaUser /></span>
+              </div>
+          </header>
+          <div className="chart-content">
+
+            <Router>
+              <Switch>
+                <Route 
+                  exact path="/" 
+                  render={() => (token ? <Redirect to="/dashboardsplash" /> : <Auth />)}
+                />
+                <ProtectedRoute
+                  // THIS MIGHT BE '/'
+                  path="/demouserlogin"
+                  component={DemoUserLogin}
+                  redirectTo="/"
+                  token={token}
+                />
+                <ProtectedRoute
+                  path="/createnewproject"
+                  component={CreateNewProject}
+                  redirectTo="/"
+                  token={token}
+                />
+                <ProtectedRoute
+                  path="/dashboardsplash"
+                  component={DashboardSplash}
+                  redirectTo="/"
+                  token={token}
+                />
+                <ProtectedRoute
+                  path="/editticket"
+                  component={EditTicket}
+                  redirectTo="/"
+                  token={token}
+                />
+                <ProtectedRoute
+                  path="/manageuserroles"
+                  component={ManageUserRoles}
+                  redirectTo="/"
+                  token={token}
+                />
+                <ProtectedRoute
+                  path="/projectdetails"
+                  component={ProjectDetails}
+                  redirectTo="/"
+                  token={token}
+                />
+                <ProtectedRoute
+                  path="/ticketdetails"
+                  component={TicketDetails}
+                  redirectTo="/"
+                  token={token}
+                />
+                <ProtectedRoute
+                  path="/tickethistoryandattachment"
+                  component={TicketHistoryAndAttachment}
+                  redirectTo="/"
+                  token={token}
+                />
+                <ProtectedRoute
+                  path="/yourtickets"
+                  component={YourTickets}
+                  redirectTo="/"
+                  token={token}
+                />
+              </Switch>
+          </Router>
+          
+          </div>
+     
+      </div>
+          
+      
     </main>
   );
 }
--- client/src/pages/DashboardSplash.jsx
+++ client/src/pages/DashboardSplash.jsx
@@ -1,26 +1,152 @@
-import React from 'react'
+import React, {Component} from 'react'
+import CanvasJSReact from '../canvasjs.react'
+let CanvasJS = CanvasJSReact.CanvasJS
+let CanvasJSChart = CanvasJSReact.CanvasJSChart 
 
-export default () => {
+
+
+export default ()  => {
+
+    function addSymbols(e){
+		var suffixes = ["", "K", "M", "B"];
+		var order = Math.max(Math.floor(Math.log(e.value) / Math.log(1000)), 0);
+		if(order > suffixes.length - 1)
+			order = suffixes.length - 1;
+		var suffix = suffixes[order];
+		return CanvasJS.formatNumber(e.value / Math.pow(1000, order)) + suffix;
+	}
+
+    const barChartOptions = {
+        // animationEnabled: true,
+        theme: "light2",
+        title:{
+            text: "N/A"
+        },
+        axisX: {
+            title: "Social Network",
+            reversed: true,
+        },
+        axisY: {
+            title: "Monthly Active Users",
+            includeZero: true,
+            labelFormatter: addSymbols, 
+        },
+        data: [{
+            type: "column",
+            dataPoints: [
+                { y:  2200000000, label: "Facebook" },
+                { y:  1800000000, label: "YouTube" },
+                { y:  800000000, label: "Instagram" },
+                { y:  563000000, label: "Qzone" },
+                { y:  376000000, label: "Weibo" },
+                { y:  336000000, label: "Twitter" },
+                { y:  330000000, label: "Reddit" }
+            ]
+        }]
+    }
+
+    const bugsErrorsPieChartOptions = {
+        // animationEnabled: true,
+        exportFileName: "New Year Resolutions",
+        exportEnabled: true,
+        title:{
+            text: "Bugs/Errors"
+        },
+        data: [{
+            type: "pie",
+            showInLegend: true,
+            legendText: "{label}",
+            toolTipContent: "{label}: <strong>{y}%</strong>",
+            indexLabel: "{y}%",
+            indexLabelPlacement: "inside",
+            dataPoints: [
+                { y: 32, label: "Health" },
+                { y: 22, label: "Finance" },
+                { y: 15, label: "Education" },
+                { y: 19, label: "Career" },
+                { y: 5, label: "Family" },
+                { y: 7, label: "Real Estate" }
+            ]
+        }]
+    }
+
+    const pieChartOptions = {
+        // animationEnabled: true,
+        exportFileName: "New Year Resolutions",
+        exportEnabled: true,
+        title:{
+            text: "Daniel Dean 5"
+        },
+        data: [{
+            type: "pie",
+            showInLegend: true,
+            legendText: "{label}",
+            toolTipContent: "{label}: <strong>{y}%</strong>",
+            indexLabel: "{y}%",
+            indexLabelPlacement: "inside",
+            dataPoints: [
+                { y: 32, label: "Health" },
+                { y: 22, label: "Finance" },
+                { y: 15, label: "Education" },
+                { y: 19, label: "Career" },
+                { y: 5, label: "Family" },
+                { y: 7, label: "Real Estate" }
+            ]
+        }]
+    }
+
+
+  
+
     return (
-    <div>
-        <header></header>
-        <div className="dashboard-graphs">
-            <div className="tickets-by-priority">
-                Graph goes here (bar chart)
-                <h1>Tickets by Priority</h1>
+    <div className="dashboard-splash">
+
+     
+
+        <div className="pie-grid">
+
+
+
+            <div>
+                <CanvasJSChart options={barChartOptions} />
+                <h2 className="tickets-by">Tickets By Priority</h2>
             </div>
-            <div className="tickets-by-type">
-                Graph goes here (pie chart)
-                <h1>Tickets by Type</h1>
+            <div>
+                <CanvasJSChart options={bugsErrorsPieChartOptions} />
+                <h2 className="tickets-by">Tickets by Type</h2>
             </div>
-            <div className="tickets-by-status">
-                Graph goes here (bar chart)
-                <h1>Tickets by Status</h1>
+            
+
+
+        </div>
+
+   
+
+        <div className="pie-grid">
+
+
+
+            <div>
+                <CanvasJSChart options={barChartOptions} />
+                <h2 className="tickets-by">Tickets by Status</h2>
             </div>
-            <div className="tickets-by-developer">
-                Graph goes here (pie chart)
-                <h1>Tickets by Developer</h1>
+            <div>
+                <CanvasJSChart options={pieChartOptions} />
+                <h2 className="tickets-by">Tickets by Developer</h2>
             </div>
+
+
+
         </div>
     </div>)
 }
+
+// Suggestable Types
+// Task. A task represents work that needs to be done.
+// Subtask. A subtask is a piece of work that is required to complete a task. ...
+// Epic. A big user story that needs to be broken down. ...
+// Bug. A bug is a problem which impairs or prevents the functions of a product.
+// Story. ...
+// Task. ...
+// Subtask. ...
+// Change.
